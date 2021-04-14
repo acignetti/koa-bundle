06b58b5997d9d48486d24acdf027931957b8fef0
Agustín Cignetti <agustin@cign.io>
2021-04-13 23:34:35 -0300

    Add auth middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import Koa from 'koa';
+import Koa from "koa";
 
-import router from './middlewares/router.js';
+import router from "./middlewares/router.js";
+import auth from "./middlewares/auth.js";
 
 const app = new Koa();
 
+app.use(auth);
 app.use(router);
 
 app.listen(3000);
---
+++ src/middlewares/auth.js
@@ -0,0 +1,17 @@
+const ACCESS_TOKEN_HEADER = "x-bundle-access-token"
+
+const hasValidHeaders = (headers) => {
+    let hasValidHeaders = false;
+    if (ACCESS_TOKEN_HEADER in headers) {
+        hasValidHeaders = headers[ACCESS_TOKEN_HEADER] == 'token'
+    }
+    return hasValidHeaders;
+}
+
+export default async (ctx, next) => {
+    if (!hasValidHeaders(ctx.request.headers)) {
+        ctx.status = 403;
+        return;
+    }
+    await next()
+}
