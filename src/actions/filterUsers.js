a1734d27c9ef4fbeb7d4f18f494308e4b2fb8f4f
Agustín Cignetti <agustin@cign.io>
2021-04-13 21:33:16 -0300

    Filter by email

--- src/actions/filterUsers.js
+++ src/actions/filterUsers.js
@@ -1,9 +1,9 @@
 import users from "../domain/users.js";
 
-const filterByEmail = users => {
-    return users
-}
+const filterByEmail = (users, filter) => filter ? users.filter(user => user.email.toLowerCase().includes(filter.toLowerCase())) : users
 
 export default async ctx => {
-    ctx.body = filterByEmail(users);
+    const query = ctx.query;
+    let response = filterByEmail(users, query.emailContains);
+    ctx.body = response;
 }
