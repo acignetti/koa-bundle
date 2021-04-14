6a174a948e6c2c1b1f57f7bc5a62e788351826aa
Agustín Cignetti <agustin@cign.io>
2021-04-13 22:45:45 -0300

    Add filtering by distance and position
    This includes a function taken directly from stackoverflow that uses the
    havershine formula

--- src/actions/filterUsers.js
+++ src/actions/filterUsers.js
@@ -1,6 +1,15 @@
 import users from "../domain/users.js";
+import havershine from "../helper/havershine.js"
 
 const filterByEmail = (users, filter) => filter ? users.filter(user => user.email.toLowerCase().includes(filter.toLowerCase())) : users
+
+const filterByPosition = (users, coordinates, radius = 10) => {
+    if (!coordinates) return users;
+    return users.filter(user => {
+        const distance = havershine(coordinates[0], coordinates[1], user.address.geo.lat, user.address.geo.lng)
+        if (distance <= radius) return user
+    })
+}
 
 const reduceToFields = (users, fields) => {
     if (!fields) return users;
@@ -15,5 +24,6 @@ const reduceToFields = (users, fields) => {
 export default async ctx => {
     const query = ctx.query;
     let filteredUsers = filterByEmail(users, query.emailContains);
+    filteredUsers = filterByPosition(filteredUsers, query.coordinate, query.radius)
     ctx.body = reduceToFields(filteredUsers, query.fields);
 }
