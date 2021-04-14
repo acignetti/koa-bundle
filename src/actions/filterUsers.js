2cc27cbacfb326222bd2ff15074ded163044c766
Agustín Cignetti <agustin@cign.io>
2021-04-13 22:04:17 -0300

    Reduce fields to what the user requested

--- src/actions/filterUsers.js
+++ src/actions/filterUsers.js
@@ -2,8 +2,18 @@ import users from "../domain/users.js";
 
 const filterByEmail = (users, filter) => filter ? users.filter(user => user.email.toLowerCase().includes(filter.toLowerCase())) : users
 
+const reduceToFields = (users, fields) => {
+    if (!fields) return users;
+
+    return users.map(user => {
+        let newUser = {}
+        fields.forEach(element => newUser[element] = user[element]);
+        return newUser
+    })
+}
+
 export default async ctx => {
     const query = ctx.query;
-    let response = filterByEmail(users, query.emailContains);
-    ctx.body = response;
+    let filteredUsers = filterByEmail(users, query.emailContains);
+    ctx.body = reduceToFields(filteredUsers, query.fields);
 }
